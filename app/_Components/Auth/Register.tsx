ee8a47e02db7d99136ab7c785064d3996be00189
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-05-27 19:15:02 +0300

    install react hook form - update register with it

--- app/_Components/Auth/Register.tsx
+++ app/_Components/Auth/Register.tsx
@@ -4,106 +4,128 @@ import React, { FC, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useToast } from "@chakra-ui/react";
 import postData from "@/app/Requests/postData";
+import { useForm } from "react-hook-form";
+
+interface FormValues{
+  username: string;
+  email: string;
+  phone: string
+  password: string;
+  c_password: string;
+}
 
 const Register: FC = () => {
+  const form = useForm<FormValues>();
+
+  const { register,handleSubmit,formState } = form;
+  const {errors} = formState
+
   const msg = useToast();
 
   const [isSubmit, setIsSubmit] = useState(false);
 
-  const phone = useRef<string>(""),
-    name = useRef<string>(""),
-    password = useRef<string>(""),
-    c_pass = useRef<string>(""),
-    mail = useRef<string>("");
-
   const redirect = useRouter();
 
-  const loginFunc = async (e: { preventDefault: () => void }) => {
+  const loginFunc = async (data: FormValues) => {
+    const {username,email,phone,password,c_password} = data;
     setIsSubmit(true);
-    e.preventDefault();
-    if (phone.current && password.current) {
-      const data = {
-        name: name.current,
-        phone: phone.current,
-        password: password.current,
-        c_password: c_pass.current,
-        email: mail.current,
-        guard: "student",
-      };
 
-      let res = await postData("register", data);
+      const dataValues = {
+        name: username,
+        phone: phone,
+        password: password,
+        c_password: c_password,
+        email: email,
+        guard: "student",
+      }
+
+
+      let res = await postData("register", dataValues);
 
       if (res.state == "error") {
         setIsSubmit(false);
         msg({ title: res.message, status: "error", duration: 3000 });
       } else {
         redirect.push("/profile");
-        // setIsSubmit(false);
+        setIsSubmit(false);
       }
       console.log(res);
-    } else {
-      setIsSubmit(false);
-      // Handle case when phone or password is undefined
-    }
+    
   };
 
   return (
-    <div>
+    <div className="register-wrapper">
       {/* <h3>Register Page</h3> */}
-      <h1 className="mt-8 mb-3 center bg-slate-200 rounded-md bold p-5">Register</h1>
-
+      <h1 className="mt-8 mb-3 center bg-slate-200 rounded-md bold p-5">
+        Register
+      </h1>
 
       <form
+      // onInvalid
+      noValidate
         className="bg-slate-50 grid lg:grid-cols-2 md:grid-cols-2 place-items-center rounded-2xl gap-5 p-3"
-        onSubmit={loginFunc}
+        onSubmit={handleSubmit(loginFunc)}
       >
         <div>
           <label htmlFor="name">Name</label>
           <input
-            name="username"
-            onChange={(e) => (name.current = e.target.value)}
+            {...register("username",{required:'user name required'})}
             type="text"
             className="p-2 bg-slate-500 outline-none border-none block rounded-lg"
           />
+          <p>{errors.username?.message}</p>
         </div>
 
         <div>
           <label htmlFor="email">Email</label>
           <input
-            name="useremail"
-            onChange={(e) => (mail.current = e.target.value)}
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Invalid email format",
+            },
+          })}
             type="email"
             className="p-2 bg-slate-500 outline-none border-none block rounded-lg"
           />
+          <p>{errors.email?.message}</p>
+
         </div>
 
         <div>
           <label htmlFor="phone">Phone</label>
           <input
-            onChange={(e) => (phone.current = e.target.value)}
+            {...register("phone",{pattern: {
+              value: /^\d{12}$/, // Matches 10-digit phone numbers
+              message: "Invalid phone number format",
+            },})}
             type="phone"
             className="p-2 bg-slate-500 outline-none border-none block rounded-lg"
           />
+          <p>{errors.phone?.message}</p>
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
           <input
-            name="password"
-            onChange={(e) => (password.current = e.target.value)}
+            {...register("password",{required:'password required'})}
             type="password"
             className="p-2 bg-slate-500 outline-none border-none block rounded-lg"
           />
+          <p>{errors.password?.message}</p>
+
         </div>
 
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
           <input
-            name="confirmPassword"
-            onChange={(e) => (c_pass.current = e.target.value)}
-            type="text"
+            {...register("c_password",{required:'confirm password required'})}
+            type="password"
             className="p-2 bg-slate-500 outline-none border-none block rounded-lg"
           />
+          <p>{errors.c_password?.message}</p>
+
         </div>
         {isSubmit ? (
           "Sending .........."
